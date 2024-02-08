be6903b49adeb35d8e1b32a10e6baaeb78b26820
emerson-moura <emerson.moura@unico.io>
2024-02-07 22:46:55 -0300

    Add ktor dependency

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -27,6 +27,9 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
         }
     }
 }
