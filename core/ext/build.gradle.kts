372817a6b6d662c319834395f6679aff66baa9e1
emerson-moura <emerson.moura@unico.io>
2024-02-09 11:51:11 -0300

    Add core ext module

---
+++ core/ext/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "Shared"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlinx.datetime)
+        }
+        androidMain.dependencies {
+        }
+    }
+}
+
+android {
+    namespace = "com.emerson.sample.news.core.ext"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
